091f0c09f484f870711c5fdd4867786242626fe2
Vincent Liao <vincent.liao@protonmail.com>
2022-02-18 14:04:07 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let name;
+</script>
+
+<main class="container">
+	<div class="nav">
+		<h1>anonroom</h1>
+		<p>about</p>
+	</div>
+
+	<div class="content">
+		<p>content1</p>
+		<p>conten2</p>
+		<p>content3</p>
+	</div>
+
+	<div class="input-box">
+		<input type="text">
+		<button>send</button>
+	</div>
+</main>
+
+<style>
+	.container {
+	  max-width: 55ch;
+		margin: 0 auto;
+		padding: 1rem;	
+	}
+
+	.nav {
+		display: flex;
+		margin: 0 auto;
+		padding: 1rem;
+	}
+
+	.content {
+		height: 100vh;
+		display: flex;
+		flex-direction: column-reverse;
+		justify-content: flex-start;
+	}
+
+</style>
