f645131c03b4d7ea2c89086bf1f6273c6fdbe7f2
Vincent Liao <vincent.liao@protonmail.com>
2022-02-18 16:19:50 +0700

    make each list item box shaped

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 		"This is fourth text",
 	];
 
-	function submit(event) {
+	function submit() {
 		texts.push(message);
 		// force re-render
 		texts = texts;
@@ -19,40 +19,36 @@
 </script>
 
 <main>
-	<div class="d-flex justify-content-center">
-		<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 576px; height: 100vh;">
-			<div class="d-flex justify-content-between mb-3">
-			<a href="/" class="d-flex p-3 link-dark text-decoration-none">
-				<span class="fw-bold">anonroom</span>
-			</a>
+	<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="max-width: 576px; margin: auto; height: 100vh;">
+		<div class="d-flex justify-content-between mb-3">
+		<a href="/" class="d-flex p-3 link-dark text-decoration-none">
+			<span class="fw-bold">anonroom</span>
+		</a>
 
-			<!-- todo: point to github -->
-			<a href="https://github.com" class="d-flex p-3 link-dark text-decoration-none">
-				<span class="fw-light">about</span>
-			</a>
-			</div>
-
-
-			<div class="list-group list-group-flush border-bottom overflow-auto">
-				{#each texts as text}
-					<div class="list-group-item py-3 lh-tight" aria-current="true">
-						<div class="d-flex w-100 align-items-center justify-content-between">
-							<span class="fw-bold">anon says:</span>
-							<span class="fw-light">Wed</span>
-						</div>
-						<div class="col-10 mb-1 small">{text}</div>
-					</div>
-				{/each}
-			</div>
-
-			<form on:submit|preventDefault="{submit}">
-				<div class="input-group mb-3"> 
-					<input type="text" class="form-control" bind:value="{message}" placeholder="What's on your mind, anon?"> 
-					<button class="btn btn-outline-secondary" type="submit">Send</button>
-				</div>
-			
-			</form>
+		<!-- todo: point to github -->
+		<a href="https://github.com" class="d-flex p-3 link-dark text-decoration-none">
+			<span class="fw-light">about</span>
+		</a>
 		</div>
 
+
+		<div class="list-group overflow-auto">
+			{#each texts as text}
+				<div class="list-group-item py-3 lh-tight" aria-current="true">
+					<div class="d-flex w-100 align-items-center justify-content-between">
+						<span class="fw-bold">anon says:</span>
+						<span class="fw-light">Wed</span>
+					</div>
+					<div class="col-10 mb-1 small">{text}</div>
+				</div>
+			{/each}
+		</div>
+
+		<form on:submit|preventDefault="{submit}">
+			<div class="input-group mb-3"> 
+				<input type="text" class="form-control" bind:value="{message}" placeholder="What's on your mind, anon?"> 
+				<button class="btn btn-outline-secondary" type="submit">Send</button>
+			</div>
+		</form>
 	</div>
 </main>
