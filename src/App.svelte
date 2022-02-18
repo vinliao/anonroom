94271c0859507224f9931afdb57cff5e21e80c9f
Vincent Liao <vincent.liao@protonmail.com>
2022-02-18 15:33:40 +0700

    center the whole thing

--- src/App.svelte
+++ src/App.svelte
@@ -10,29 +10,39 @@
 </script>
 
 <main>
-	<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 380px;">
-		<a href="/" class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom">
-			<span class="fs-5 fw-semibold">anonroom</span>
-		</a>
+	<div class="d-flex justify-content-center">
+		<div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white" style="width: 380px;">
+			<div class="d-flex justify-content-between">
+			<a href="/" class="d-flex p-3 link-dark text-decoration-none">
+				<span class="fw-bold">anonroom</span>
+			</a>
+
+			<!-- todo: point to github -->
+			<a href="https://github.com" class="d-flex p-3 link-dark text-decoration-none">
+				<span class="fw-light">about</span>
+			</a>
+			</div>
 
 
-		<div class="list-group list-group-flush border-bottom scrollarea">
-			{#each texts as text}
-				<a href="#" class="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
-					<div class="d-flex w-100 align-items-center justify-content-between">
-						<strong class="mb-1">anon says:</strong>
-						<small>Wed</small>
+			<div class="list-group list-group-flush border-bottom scrollarea">
+				{#each texts as text}
+					<div class="list-group-item py-3 lh-tight" aria-current="true">
+						<div class="d-flex w-100 align-items-center justify-content-between">
+							<span class="fw-bold">anon says:</span>
+							<span class="fw-light">Wed</span>
+						</div>
+						<div class="col-10 mb-1 small">{text}</div>
 					</div>
-					<div class="col-10 mb-1 small">{text}</div>
-				</a>
-			{/each}
+				{/each}
+			</div>
+
+			<form on:submit|preventDefault="{submit}">
+				<div class="input-group"> <input type="text" class="form-control" placeholder="What's on your mind, anon?" aria-label="Recipient's username" aria-describedby="button-addon2"> 
+					<button class="btn btn-outline-secondary" type="submit" id="button-addon2">Send</button>
+				</div>
+			
+			</form>
 		</div>
 
-		<form on:submit|preventDefault="{submit}">
-			<div class="input-group"> <input type="text" class="form-control" placeholder="What's on your mind, anon?" aria-label="Recipient's username" aria-describedby="button-addon2"> 
-				<button class="btn btn-outline-secondary" type="submit" id="button-addon2">Send</button>
-			</div>
-		
-		</form>
 	</div>
 </main>
