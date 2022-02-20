c68bc5b5b9ecd94b20d829170199fe5327d4c3c2
Vincent Liao <vincent.liao@protonmail.com>
2022-02-21 01:00:25 +0700

    implement ui reply

--- src/App.svelte
+++ src/App.svelte
@@ -85,7 +85,7 @@
 			<div class="flex">
 				<div class="flex-1"></div>
 				<a href="https://github.com/vinliao/anonroom" class="text-right mb-12">
-					<span class="font-light font-mono">about</span>
+					<span class="font-light font-mono underline">about</span>
 				</a>
 			</div>
 
@@ -97,12 +97,42 @@
 			</div>
 		</form>
 
+		<!-- an example of what the reply ui looks like -->
+		<div class="flex justify-between">
+			<span class="font-bold">anon says:</span>
+			<span class="font-light">2 minutes ago</span>
+		</div>
+		<div class="mb-2">the reply three two one</div>
+
+		<div class="ml-5 p-3 mb-2 bg-slate-50">
+			<div class="flex justify-between">
+				<span class="font-bold">anon says:</span>
+				<span class="font-light">2 minutes ago</span>
+			</div>
+			<div>the replied one two three</div>
+
+		</div>
+
+		<div class="flex mb-10">
+			<a href="/">
+				<span class="font-mono underline">reply</span>
+			</a>
+			<div class="flex-1"></div>
+		</div>
+
+
 		{#each tweets as tweet}
 			<div class="flex justify-between">
 				<span class="font-bold">anon says:</span>
 				<span class="font-light">{tweet.time}</span>
 			</div>
-			<div class="mb-8">{tweet.message}</div>
+			<div class="mb-2">{tweet.message}</div>
+			<div class="flex mb-10">
+				<a href="/">
+					<span class="font-mono underline">reply</span>
+				</a>
+				<div class="flex-1"></div>
+			</div>
 		{/each}
 	</div>
 </main>
