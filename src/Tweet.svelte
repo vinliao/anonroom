49405de6bf0de6058997532c0d5390f69dc55f2a
Vincent Liao <vincent.liao@protonmail.com>
2022-02-21 13:40:57 +0700

    make timeago text lighter

--- src/Tweet.svelte
+++ src/Tweet.svelte
@@ -6,14 +6,14 @@
 
 <div class="flex justify-between">
   <span class="font-bold">anon says:</span>
-  <span class="font-light pr-3">{time}</span>
+  <span class="font-light text-slate-500 pr-3">{time}</span>
 </div>
 <div class="mb-2">{message}</div>
 {#if replied}
   <div class="ml-5 p-3 mb-2 bg-gray-50">
     <div class="flex justify-between">
       <span class="font-bold">anon says:</span>
-      <span class="font-light">{replied.time}</span>
+      <span class="font-light text-slate-500">{replied.time}</span>
     </div>
     <div>{replied.message}</div>
   </div>
